af9115898d1d68c3d3bc3badd4a21d396a72d172
ウエスリー <wesley@turnip.evokateur.net>
2025-10-24 15:52:08 -0700

    oops

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ uv self update
 echo "Running uv sync to install dependencies.."
 uv sync
 
-echo "Installing Jupyter kernel spec for $(pwd)/llms.."
+echo "Installing Jupyter kernel spec for $(pwd)/.venv.."
 python -m ipykernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=llm_engineering --display-name "LLM Engineering"
 echo
 echo "To launch Jupyter Lab in the uv environment:"
