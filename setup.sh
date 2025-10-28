b0b1c50d5865fec9a93d2ef4171fa068c7cf27c9
ウエスリー <wesley@cipolla.evokateur.net>
2025-10-27 19:55:42 -0700

    tersify echo in setup.sh

--- setup.sh
+++ setup.sh
@@ -9,10 +9,10 @@ uv sync
 echo "Installing Jupyter kernel spec for $(pwd)/.venv.."
 python -m ipykernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=llm_engineering --display-name "LLM Engineering"
 echo
+echo "Use the 'llm_engineering' kernel in Jupyter notebooks."
+echo
 echo "To launch Jupyter Lab in the uv environment:"
 echo " uv run --with jupyter jupyter lab"
 echo
 echo "To launch nvim in the uv environment (for molten.nvim):"
 echo " uv run nvim"
-echo
-echo "Select the 'llm_engineering' kernel when working with notebooks in the web UI or running :MoltenInit"
