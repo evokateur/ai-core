979e9e01df72b28cfb1467f7291c35f5684b2845
ウエスリー <wesley@sfizio.evokateur.net>
2025-10-30 14:52:20 -0700

    oops

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ echo "Running uv sync to install dependencies.."
 uv sync
 
 echo "Installing Jupyter kernel spec for $(pwd)/.venv.."
-python -m ipykernel install --user --env VIRTUAL_ENV $(pwd)/.venv --name=llm_engineering --display-name "LLM Engineering"
+uv run python -m ipykernel install --user --env VIRTUAL_ENV "$(pwd)/.venv" --name=llm_engineering --display-name "LLM Engineering"
 echo
 echo "Use the 'llm_engineering' kernel in Jupyter notebooks."
 echo
